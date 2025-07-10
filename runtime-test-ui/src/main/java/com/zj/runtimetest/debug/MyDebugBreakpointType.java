ce6f46ba1122b63e63646c2c5faed69ee8063f34
jie.zhou <jie.zhou@alwaysmkt.com>
2025-07-10 17:49:46 +0800

    调整

--- runtime-test-ui/src/main/java/com/zj/runtimetest/debug/MyDebugBreakpointType.java
+++ runtime-test-ui/src/main/java/com/zj/runtimetest/debug/MyDebugBreakpointType.java
@@ -2,18 +2,17 @@ package com.zj.runtimetest.debug;
 
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.xdebugger.breakpoints.XLineBreakpoint;
 import com.intellij.xdebugger.breakpoints.XLineBreakpointType;
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider;
-import com.zj.runtimetest.ui.RuntimeTestAction;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.java.debugger.JavaDebuggerEditorsProvider;
 
-import javax.swing.*;
-
+/**
+ * @author jie.zhou
+ */
 public class MyDebugBreakpointType extends XLineBreakpointType<MyBreakpointProperties> {
 
     public static final String ID = "my-debug-breakpoint";
@@ -31,46 +30,6 @@ public class MyDebugBreakpointType extends XLineBreakpointType<MyBreakpointPrope
     @Override
     public boolean isSuspendThreadSupported() {
         return true;
-    }
-
-    @Override
-    public Icon getTemporaryIcon() {
-        return IconLoader.getIcon("/icons/logo.svg", RuntimeTestAction.class.getClassLoader());
-    }
-
-    @Override
-    public @NotNull Icon getEnabledIcon() {
-        return IconLoader.getIcon("/icons/logo.svg", RuntimeTestAction.class.getClassLoader());
-    }
-
-    @Override
-    public @NotNull Icon getDisabledIcon() {
-        return IconLoader.getIcon("/icons/logo.svg", RuntimeTestAction.class.getClassLoader());
-    }
-
-    @Override
-    public @NotNull Icon getSuspendNoneIcon() {
-        return IconLoader.getIcon("/icons/logo.svg", RuntimeTestAction.class.getClassLoader());
-    }
-
-    @Override
-    public @NotNull Icon getMutedEnabledIcon() {
-        return IconLoader.getIcon("/icons/logo.svg", RuntimeTestAction.class.getClassLoader());
-    }
-
-    @Override
-    public @NotNull Icon getMutedDisabledIcon() {
-        return IconLoader.getIcon("/icons/logo.svg", RuntimeTestAction.class.getClassLoader());
-    }
-
-    @Override
-    public @Nullable Icon getPendingIcon() {
-        return IconLoader.getIcon("/icons/logo.svg", RuntimeTestAction.class.getClassLoader());
-    }
-
-    @Override
-    public @NotNull Icon getInactiveDependentIcon() {
-        return IconLoader.getIcon("/icons/logo.svg", RuntimeTestAction.class.getClassLoader());
     }
 
     @Override
