b31d890ba6e89b5db8e7d2af894f56f2cb79fadf
zhoujie <wwsbdy@163.com>
2025-07-31 23:05:17 +0800

    兼容201版本

--- runtime-test-ui/src/main/java/com/zj/runtimetest/ui/script/ScriptToolWindowFactory.java
+++ runtime-test-ui/src/main/java/com/zj/runtimetest/ui/script/ScriptToolWindowFactory.java
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.NotNull;
 public class ScriptToolWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        ContentFactory contentFactory = ContentFactory.getInstance();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(new ScriptEditorPanel(project).getMainPanel(), "", false);
         toolWindow.getContentManager().addContent(content);
     }
